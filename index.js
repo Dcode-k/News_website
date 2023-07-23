da0119a0eadb0adb05b31c9f7d398dae907912e9
Dcode-k <Krishnkantpachori50@gmail.com>
2023-07-23 13:15:58 +0530

    add new news api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("this is index js file");
-let apiKey = "0552b11b6bfe4156bb0b8a676ce40415";
+let apiKey = "pub_2659581f55f944d72ca138b2f1727420b1fec";
 let source = "bbc-news";
-let country = "in";
+let language="en"
 let category = "technology";
 // grab the news container
 let newsAccordion = document.getElementById("newsAccordion");
@@ -10,21 +10,21 @@ let newsAccordion = document.getElementById("newsAccordion");
 const xhr = new XMLHttpRequest();
 xhr.open(
   "GET",
-  `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apiKey}`
+  `https://newsdata.io/api/1/news?apikey=${apiKey}&language=${language}`
 );
 
 // What to do when response is ready
 xhr.onload = function () {
   if (this.status === 200) {
     let json = JSON.parse(this.responseText);
-    //    console.log(this.responseText);
-    //    console.log(json);
-    let articles = json.articles;
+      //  console.log(this.responseText);
+      //  console.log(json);
+    let articles = json.results;
     console.log(articles);
     let newsHTML=``;
     articles.forEach(function(element,index){
 
-         console.log(element,index);
+        //  console.log(element,index);
             let news = `<div class="accordion accordion-flush" id="newsAccordion">
             <div class="accordion-item">
             <h2 class="accordion-header" id="flush-heading${index}">
@@ -33,7 +33,7 @@ xhr.onload = function () {
             </button>
             </h2>
             <div id="flush-collapse${index}" class="accordion-collapse collapse" aria-labelledby="flush-heading${index}" data-bs-parent="#newsAccordion">
-            <div class="accordion-body">${element.content}. <a href="${element["url"]}" target="_blank">Read more here</a></div>
+            <div class="accordion-body">${element.content.slice(0,1000)}. <a href="${element.link}" target="_blank">Read more here</a></div>
             </div>
             </div>
             
